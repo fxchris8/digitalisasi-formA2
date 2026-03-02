ea16eaa18deb495ba754944901b819eeeaa759d7
Hilmi Fawwaz Sa'ad <128681954+hilmifawwazsaad@users.noreply.github.com>
2026-03-02 15:11:14 +0700

    feat: expand Toaster component in main.tsx for enhanced notifications

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,7 +13,7 @@ createRoot(rootElement).render(
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
-      <Toaster richColors={true} position="top-right" />
+      <Toaster richColors={true} position="top-right" expand={true} />
     </AuthProvider>
   </StrictMode>,
 )
