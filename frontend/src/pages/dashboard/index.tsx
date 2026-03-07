378e89575c14aaf71d6989c7146b3320c09bc22f
Hilmi Fawwaz Sa'ad <128681954+hilmifawwazsaad@users.noreply.github.com>
2026-03-07 09:49:43 +0700

    feat: update DashboardPage to include ManagerView and StaffView, and improve user role checks

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,14 +1,17 @@
 import { useAuth } from "@/contexts/auth.context"
-import { ROLES } from "@/lib/rbac"
+import { getManagerStep, ROLES } from "@/lib/rbac"
 import AdminView from "./_views/admin-view"
-import GeneralView from "./_views/general-view"
+import ManagerView from "./_views/manager-view"
+import StaffView from "./_views/staff-view"
 import UserView from "./_views/user-view"
 
 export default function DashboardPage() {
   const { user } = useAuth()
 
-  if (user?.role === ROLES.ADMIN) return <AdminView />
-  if (user?.role === ROLES.USER) return <UserView />
+  if (!user) return null
+  if (user.role === ROLES.ADMIN) return <AdminView />
+  if (user.role === ROLES.USER) return <UserView />
+  if (getManagerStep(user)) return <ManagerView />
 
-  return <GeneralView />
+  return <StaffView />
 }
