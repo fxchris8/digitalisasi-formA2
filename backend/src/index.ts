1e8dec2e8c4a082dfba453e0c0708b71764ea3c4
Hilmi Fawwaz Sa'ad <128681954+hilmifawwazsaad@users.noreply.github.com>
2026-03-16 16:16:49 +0700

    feat: add startScheduler for seaman to backend main endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import "@/config/env"
 import app from "@/app"
+import { startScheduler } from "@/scheduler"
 
 const PORT = Number(process.env.PORT) || 3000
 
 app.listen(PORT, () => {
   console.log(`[START] Server running on http://localhost:${PORT}`)
+  startScheduler()
 })
